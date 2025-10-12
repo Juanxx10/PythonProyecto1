bab75feaa3c5379a6b78ac30708fe6782343c65a
Juan I. Montano <jmonty2772@gmail.com>
2025-10-12 15:47:38 -0300

    Eliminacion de myapp

--- myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from datetime import datetime
-
-def index(request):
-    context = {
-        'mensaje': 'Bienvenidos a mi aplicación Django',
-        'year': datetime.now().year
-    }
-    return render(request, 'myapp/index.html', context)
