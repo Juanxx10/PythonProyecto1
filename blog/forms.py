dce2a3af1241cfdc20a6678d51eba0ae661989fd
Juan I. Montano <jmonty2772@gmail.com>
2025-10-13 20:20:45 -0300

    Cambios generales

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['titulo', 'contenido', 'estado']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'contenido': forms.Textarea(attrs={'class': 'form-control'}),
+            'estado': forms.Select(attrs={
+                'class': 'form-select',
+                'style': 'max-width: 150px;'
+            }),
+        }
